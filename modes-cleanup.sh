630851d6363f9cdff9e1afbfba42903234772cd5
WrightRocket <wright.keith@gmail.com>
2024-05-31 22:15:04 -0700

    Implemented creating special permissions and tested on Debian Bookworm Linux bash

---
+++ modes-cleanup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Run as root to remove all directories and files that are created
+
+rm -rf dir* file*
--- modes.sh
+++ modes.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
+# Must be run as root
 # Show all modes of files and directories
-# mode is octal values from 0 to 7
-# A mode is composed of four values SUGO
+# The mode is set with four octal values from 0 to 7
+# Modes can also be expressed as a single letter
+# represented by one of four mode letters: S U G O
 # S is special
 # U is user owner
 # G is group owner
 # O is others not the owner or group member
 
-# Not demoing special now
+# Use four loops for Special, User, Group, and Other 
+# Create files and directories
 
-# Loop through three times for Other, Group, and User
-# count the files
 octal=$(seq 0 7)
-for u in $octal 
-do
-	echo Setting u=$u permissions	
-	for g in $octal
-	do
-		echo Setting g=$g permissions	
-
-		for o in $octal
+for s in $octal 
+do 
+	echo Setting s=$s SPECIAL permissions
+	for u in $octal
+	do 
+		echo Setting u=$u USER permissions	
+		for g in $octal
 		do
-			echo Setting o=$o permissions	
-
-			perm="${u}${g}${o}"
-			touch file${perm}
-			mkdir dir${perm}
-			chmod $perm file${perm} dir${perm}
+			echo Setting g=$g GROUP permissions	
+			for o in $octal
+			do
+				echo Setting o=$o OTHER permissions	
+				perm="${s}${u}${g}${o}"
+				touch file${perm}
+				mkdir dir${perm}
+				chmod $perm file${perm} dir${perm}
+			done
 		done
 	done
 done
