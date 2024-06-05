17e2a3e3ffc937b951029cea5e52c99233f420e9
WrightRocket <wright.keith@gmail.com>
2024-06-04 22:56:18 -0700

    Update modes-view.sh
    Add comment

--- modes-view.sh
+++ modes-view.sh
@@ -2,8 +2,10 @@
 # View the contents of the archive without extracting it
 
 tar -tvf modes-dirs-files.tar.gz | cut -d " " -f1,14
-tar -tvf modes-dirs-files.tar.gz | wc -l
 # Use cut to extract the first column of text, and that after 14 spaces
+
+tar -tvf modes-dirs-files.tar.gz | wc -l
+# Display the total number of files and directories created 
 
 # Extract the contents of the archive in current directory
 # tar -xvf modes-dirs-files.tar.gz
